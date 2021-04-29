5f081317d97b61eb7c3d830bcde469ad0c19976b
Tomas <tomas.yee@gmail.com>
2021-04-29 23:38:48 +0300

    DB extended

---
+++ SmartCourier/Pages/Delivery.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartCourier.Pages
+{
+    public class PrivacyModel1 : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel1(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            string id = Request.Form["search"];
+            System.Diagnostics.Debug.WriteLine(id);
+            System.Diagnostics.Debug.WriteLine("Nepraleido");
+            return null;
+        }
+    }
+}
--- SmartCourier/Pages/Privacy.cshtml.cs
+++ SmartCourier/Pages/Privacy.cshtml.cs
@@ -16,24 +16,21 @@ namespace SmartCourier.Pages
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
-            System.Diagnostics.Debug.WriteLine("Paleido");
         }
 
         public IActionResult OnPost()
         {
             //čia išvedimas ar kodas
-            System.Diagnostics.Debug.WriteLine("Funkcija");
             Database databaseObject = new Database();
             
             string username = Request.Form["email"];
             string password = Request.Form["Password"];
             System.Diagnostics.Debug.WriteLine(username);
-            System.Diagnostics.Debug.WriteLine("LABUKAS");
             System.Diagnostics.Debug.WriteLine(password);
             databaseObject.OpenConnection();
             if (databaseObject.LogIn(username, password))
             {
-                System.Diagnostics.Debug.WriteLine("duombaze suveike");
+                Response.Redirect("Delivery");
             }
             else
             {
