1d52cf58755e591bf6738171deed5535771d0ce6
Gytautele <gytaute.barzdziute.comli@gma>
2021-05-14 14:56:01 +0300

    Added "Add dispatch" page

---
+++ SmartCourier/Pages/DispatchAdd.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartCourier.Pages
+{
+    public class DeliveryModel4 : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public DeliveryModel4(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if(Globals.username == null)
+            {
+                Response.Redirect("Privacy");
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            Database databaseObject = new Database();
+            databaseObject.OpenConnection();
+            string idMap = Request.Form["dispatch_id"];
+            System.Diagnostics.Debug.WriteLine("Data");
+            var data = databaseObject.select();
+            double x = 0;
+            double y = 0;
+            while (data.Read())
+            {
+                if (data.GetInt32(0) == Int32.Parse(idMap))
+                {
+                    x = data.GetDouble(6);
+                    y = data.GetDouble(7);
+                }
+            }
+            
+            System.Diagnostics.Debug.WriteLine(x + " " + y);
+            databaseObject.CloseConnection();
+            return null;
+        }
+    }
+}
