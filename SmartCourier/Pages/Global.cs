ebb35efc1eeb353ac7d936262e6c4942b3f2d6d4
Tomas <tomas.yee@gmail.com>
2021-05-19 23:09:29 +0300

    gn

--- SmartCourier/Pages/Global.cs
+++
@@ -1,7 +0,0 @@
-﻿static class Globals
-{
-    public static string username;
-    public static string email = "tomas.klevas9@gmail.com";
-    public static string password = "s";
-    public static int id = 4;
-}
